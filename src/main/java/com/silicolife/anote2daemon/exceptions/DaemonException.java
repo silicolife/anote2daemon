3edf10023543f38979756e6b0f3727bb3c5fa315
Joel Costa <jcosta@silicolife.com>
2015-02-17 18:00:03 +0000

    [refactor] refactor exceptions

--- src/main/java/com/silicolife/anote2daemon/exceptions/DaemonException.java
+++ src/main/java/com/silicolife/anote2daemon/exceptions/DaemonException.java
@@ -1,18 +1,13 @@
 package com.silicolife.anote2daemon.exceptions;
 
 /**
- * Class to represent the anote2dameon exceptions
- * 
- * 
+ * Anote2Daemon exceptions
  * 
  * @author Joel Azevedo Costa
  * @year 2015
  */
 public class DaemonException extends RuntimeException {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private String code;
 
