a0996b362753bd45add1ac503240ea073033c622
Joel Costa <jcosta@silicolife.com>
2015-02-17 17:44:08 +0000

    [refactor] HibernateAwareMapper

--- src/main/java/com/silicolife/anote2daemon/webservice/HibernateAwareObjectMapper.java
+++ src/main/java/com/silicolife/anote2daemon/webservice/HibernateAwareObjectMapper.java
@@ -4,13 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
 
 public class HibernateAwareObjectMapper extends ObjectMapper {
-
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public HibernateAwareObjectMapper() {
-        registerModule(new Hibernate4Module());
-    }
+		registerModule(new Hibernate4Module());
+	}
 }
