9c952abebbfe41d435d210d25ba462de354e4687
Joel Costa <jcosta@silicolife.com>
2015-04-10 11:48:27 +0100

    [fix] Method to handler status codes server

--- src/main/java/com/silicolife/anote2daemon/exceptions/handler/ServerCodesExceptions.java
+++ src/main/java/com/silicolife/anote2daemon/exceptions/handler/ServerCodesExceptions.java
@@ -5,13 +5,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.silicolife.anote2daemon.exceptions.entities.ExceptionInfo;
 import com.silicolife.anote2daemon.webservice.DaemonResponse;
+
 /**
- * Generic class to handler with server codes exceptions, like 404
+ * Generic class to handler with server codes exceptions, like 404, 400, ...
  * 
  * @author Joel Azevedo Costa
  * @year 2015
@@ -21,7 +21,9 @@ import com.silicolife.anote2daemon.webservice.DaemonResponse;
 public class ServerCodesExceptions extends ResponseEntityExceptionHandler {
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public ResponseEntity handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+	@Override
+	public ResponseEntity handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
 		String statusCode = status.name();
 		String reason = status.getReasonPhrase();
 		String message = ex.getMessage();
@@ -29,7 +31,5 @@ public class ServerCodesExceptions extends ResponseEntityExceptionHandler {
 		DaemonResponse<?> response = new DaemonResponse<>();
 		response.setException(exception);
 		return new ResponseEntity<DaemonResponse<?>>(response, status);
-
 	}
-
 }
