888354e5f49ee2e9a356958eff411776de85ace9
Hugo Costa <hcosta@silicolife.com>
2016-04-19 18:02:29 +0100

    [update] RunServerProcesses update to receive a configurationUID and Json

--- src/main/java/com/silicolife/anote2daemon/controller/runserverprocesses/RunServerProcessesController.java
+++ src/main/java/com/silicolife/anote2daemon/controller/runserverprocesses/RunServerProcessesController.java
@@ -1,5 +1,7 @@
 package com.silicolife.anote2daemon.controller.runserverprocesses;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,9 +13,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.silicolife.anote2daemon.processes.ir.PubMedSearchServerRunExtension;
 import com.silicolife.anote2daemon.webservice.DaemonResponse;
+import com.silicolife.textmining.core.datastructures.dataaccess.database.dataaccess.implementation.exceptions.RunServerProcessesException;
 import com.silicolife.textmining.core.datastructures.dataaccess.database.dataaccess.implementation.security.Permissions;
-import com.silicolife.textmining.core.datastructures.process.ConfigurationImpl;
+import com.silicolife.textmining.core.datastructures.dataaccess.database.dataaccess.implementation.service.publications.PublicationsService;
+import com.silicolife.textmining.core.datastructures.dataaccess.database.dataaccess.implementation.service.queries.QueriesService;
+import com.silicolife.textmining.core.datastructures.exceptions.process.InvalidConfigurationException;
+import com.silicolife.textmining.core.interfaces.core.dataaccess.exception.ANoteException;
+import com.silicolife.textmining.core.interfaces.process.IR.exception.InternetConnectionProblemException;
+import com.silicolife.textmining.processes.ir.pubmed.configuration.IRPubmedSearchConfigurationImpl;
 
 /**
  * The goal of this class is to expose for the web all Publications
@@ -32,6 +44,11 @@ public class RunServerProcessesController {
 	@Autowired
 	private Permissions permissions;
 
+	@Autowired
+	private QueriesService queriesService;
+	
+	@Autowired
+	private PublicationsService publicationsService;
 
 
 	/**
@@ -42,8 +59,31 @@ public class RunServerProcessesController {
 	 */
 	@PreAuthorize("isAuthenticated()")
 	@RequestMapping(value = "/configuration", method = RequestMethod.POST, consumes = { "application/json" })
-	public ResponseEntity<DaemonResponse<Boolean>> executeConfiguration(@RequestBody ConfigurationImpl configuration) {
-
+	public ResponseEntity<DaemonResponse<Boolean>> executeConfiguration(@RequestBody String[] parameters) throws RunServerProcessesException{
+		System.out.println(parameters[0] + " " + parameters[1]);
+		ObjectMapper bla = new ObjectMapper();
+		try {
+			switch (parameters[0]) {
+				case IRPubmedSearchConfigurationImpl.pubmedsearchUID :
+					IRPubmedSearchConfigurationImpl searchConfiguration = bla.readValue(parameters[1],IRPubmedSearchConfigurationImpl.class);
+					new PubMedSearchServerRunExtension(queriesService,publicationsService).search(searchConfiguration);
+					break;
+				default :
+					break;
+			}
+		} catch (JsonParseException e) {
+			throw new RunServerProcessesException(e);
+		} catch (JsonMappingException e) {
+			throw new RunServerProcessesException(e);
+		} catch (IOException e) {
+			throw new RunServerProcessesException(e);
+		} catch (ANoteException e) {
+			throw new RunServerProcessesException(e);
+		} catch (InternetConnectionProblemException e) {
+			throw new RunServerProcessesException(e);
+		} catch (InvalidConfigurationException e) {
+			throw new RunServerProcessesException(e);
+		}
 		DaemonResponse<Boolean> response = new DaemonResponse<Boolean>(true);
 		return new ResponseEntity<DaemonResponse<Boolean>>(response, HttpStatus.OK);
 	}
