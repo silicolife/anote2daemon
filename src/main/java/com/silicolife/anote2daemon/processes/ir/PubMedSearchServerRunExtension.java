8cd230fc8d4ead9241384c988737a74e105693e6
Hugo Costa <hcosta@silicolife.com>
2016-04-19 18:03:06 +0100

    [refactoring] PubmedSearchServerRun to get dataaccess from daemon services

--- src/main/java/com/silicolife/anote2daemon/processes/ir/PubMedSearchServerRunExtension.java
+++ src/main/java/com/silicolife/anote2daemon/processes/ir/PubMedSearchServerRunExtension.java
@@ -1,13 +1,19 @@
 package com.silicolife.anote2daemon.processes.ir;
 
 import java.util.GregorianCalendar;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import com.silicolife.textmining.core.datastructures.dataaccess.database.dataaccess.implementation.service.publications.PublicationsService;
+import com.silicolife.textmining.core.datastructures.dataaccess.database.dataaccess.implementation.service.queries.QueriesService;
 import com.silicolife.textmining.core.datastructures.exceptions.process.InvalidConfigurationException;
 import com.silicolife.textmining.core.interfaces.core.dataaccess.exception.ANoteException;
 import com.silicolife.textmining.core.interfaces.core.document.IPublication;
 import com.silicolife.textmining.core.interfaces.core.report.processes.ir.IIRSearchProcessReport;
 import com.silicolife.textmining.core.interfaces.process.IR.IIRSearchConfiguration;
+import com.silicolife.textmining.core.interfaces.process.IR.IQuery;
 import com.silicolife.textmining.core.interfaces.process.IR.exception.InternetConnectionProblemException;
 import com.silicolife.textmining.core.interfaces.process.utils.ISimpleTimeLeft;
 import com.silicolife.textmining.processes.ir.pubmed.PubMedSearch;
@@ -20,11 +26,17 @@ import com.silicolife.textmining.processes.ir.pubmed.PubMedSearch;
  *
  */
 public class PubMedSearchServerRunExtension extends PubMedSearch {
-
+	
 	private long starttime;
+	
+	private QueriesService queriesService;
+	
+	private PublicationsService publicationsService;
 
-	public PubMedSearchServerRunExtension() {
+	public PubMedSearchServerRunExtension(QueriesService queriesService, PublicationsService publicationsService) {
 		super();
+		this.queriesService=queriesService;
+		this.publicationsService=publicationsService;
 	}
 
 	public IIRSearchProcessReport search(IIRSearchConfiguration searchConfiguration) throws ANoteException, InternetConnectionProblemException, InvalidConfigurationException {
@@ -34,6 +46,39 @@ public class PubMedSearchServerRunExtension extends PubMedSearch {
 
 	public List<IPublication> searchPublicationMetaInfoUsingPMID(List<String> pmids, ISimpleTimeLeft progress) throws InternetConnectionProblemException {
 		return super.searchPublicationMetaInfoUsingPMID(pmids);
+	}
+	
+	@Override
+	protected void registerQueryOnDatabase(IQuery query) throws ANoteException {
+		queriesService.create(query);
+	}
+	
+	@Override
+	protected Map<String, Long> getAllPublicationExternalIdFromSource(String source )throws ANoteException {
+		return publicationsService.getAllPublicationsIdFromSource(source);
+	}
+	
+	@Override
+	protected Set<String> getQueryPublicationIDWithGivenSource(IQuery query,String str) throws ANoteException {
+		return queriesService.getQueryPublicationsExternalIDFromSource(query.getId(),str);
+	}
+	
+	@Override
+	protected  void insertPublications(List<IPublication> documentsToInsert)throws ANoteException {
+		publicationsService.create(documentsToInsert);
+	}
+	
+	@Override
+	protected void insertQueryPublications(IQuery query,List<IPublication> publicationToAdd) throws ANoteException {
+		Set<Long> publicationsIds = new HashSet<Long>();
+		for (IPublication pub : publicationToAdd)
+			publicationsIds.add(pub.getId());
+		queriesService.addPublicationsToQuery(query.getId(), publicationsIds);
+	}
+	
+
+	protected void updateQueryOnDatabase(IQuery query) throws ANoteException {
+		queriesService.update(query);
 	}
 
 	@Override
