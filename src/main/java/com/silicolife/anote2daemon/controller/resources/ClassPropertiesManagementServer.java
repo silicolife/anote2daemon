a8952f623dbd48e07137d5682fd9ec68c92c90fd
Ruben Rodrigues <rrodrigues@silicolife.com>
2016-06-17 15:07:39 +0100

    [Add] Class properties Manager server.

---
+++ src/main/java/com/silicolife/anote2daemon/controller/resources/ClassPropertiesManagementServer.java
@@ -0,0 +1,91 @@
+package com.silicolife.anote2daemon.controller.resources;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import com.silicolife.textmining.core.datastructures.dataaccess.database.dataaccess.implementation.service.resources.ClassesService;
+import com.silicolife.textmining.core.interfaces.core.dataaccess.exception.ANoteException;
+import com.silicolife.textmining.core.interfaces.core.general.classe.IAnoteClass;
+
+public class ClassPropertiesManagementServer{
+
+	private static Map<Long,IAnoteClass> classIDAnoteClass = new HashMap<>();
+	private static Map<String,IAnoteClass> classNameAnoteClass = new HashMap<>();
+	
+	private static void refreshClasses(ClassesService classesService) throws ANoteException {
+		if(classesService != null){
+			Set<IAnoteClass> classes = classesService.getClasses();
+			classIDAnoteClass = new HashMap<Long, IAnoteClass>();
+			classNameAnoteClass = new HashMap<>();
+			for(IAnoteClass anoteKlass:classes)
+			{
+				classIDAnoteClass.put(anoteKlass.getId(), anoteKlass);
+				classNameAnoteClass.put(anoteKlass.getName().toLowerCase(), anoteKlass);
+			}
+		}
+	}
+	
+	public static IAnoteClass getClassIDOrinsertIfNotExist(ClassesService classesService, IAnoteClass klass) throws ANoteException
+	{
+		if(classNameAnoteClass.containsKey(klass.getName().toLowerCase()))
+		{
+			return classNameAnoteClass.get(klass.getName().toLowerCase());
+		}
+		else
+		{
+			refreshClasses(classesService);
+			if(classNameAnoteClass.containsKey(klass.getName().toLowerCase()))
+			{
+				return classNameAnoteClass.get(klass.getName().toLowerCase());
+			}else{
+				classesService.addClass(klass);
+				classIDAnoteClass.put(klass.getId(), klass);
+				classNameAnoteClass.put(klass.getName().toLowerCase(), klass);
+				return klass;
+			}
+		}
+	}
+	
+	public static IAnoteClass getClassGivenClassID(ClassesService classesService, long classID) throws ANoteException
+	{
+		if(classIDAnoteClass.containsKey(classID))
+		{
+			return classIDAnoteClass.get(classID);
+		}
+		else
+		{
+			refreshClasses(classesService);
+			return classIDAnoteClass.get(classID);
+		}
+	}
+	
+	public static IAnoteClass getClassIDClassName(ClassesService classesService, String classsName) throws ANoteException
+	{
+		if(classNameAnoteClass.containsKey(classsName.toLowerCase()))
+		{
+			return classNameAnoteClass.get(classsName.toLowerCase());
+		}
+		else
+		{
+			refreshClasses(classesService);
+			return classNameAnoteClass.get(classsName.toLowerCase());
+		}
+	}
+
+	public static void updateClassName(ClassesService classesService, IAnoteClass klass, String newCLass) throws ANoteException {
+//		classesService.updateClassName(klass,newCLass);
+		refreshClasses(classesService);
+	}
+
+	public static Collection<IAnoteClass> getClasses()
+	{
+		return classNameAnoteClass.values();
+	}
+
+	public static boolean containsClass(ClassesService classesService, String newCLass) throws ANoteException {
+		return getClassIDClassName(classesService,newCLass)!=null;
+	}
+}
+
