210d3a8addef2fe07e2ade1727f7c111c609dba8
Ruben Rodrigues <rrodrigues@silicolife.com>
2016-06-22 09:07:34 +0100

    [Update] Spring runnable now dont use the http request in the thread. Only saves the user authentcation to set the userlogged in the background process.

--- src/main/java/com/silicolife/anote2daemon/utils/SpringRunnable.java
+++ src/main/java/com/silicolife/anote2daemon/utils/SpringRunnable.java
@@ -1,34 +1,48 @@
 package com.silicolife.anote2daemon.utils;
 
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.silicolife.anote2daemon.model.core.entities.CustomSpringUser;
+import com.silicolife.textmining.core.datastructures.dataaccess.database.dataaccess.implementation.model.core.dao.UsersLogged;
+import com.silicolife.textmining.core.datastructures.dataaccess.database.dataaccess.implementation.model.core.dao.UsersLoggedImpl;
 
 public abstract class SpringRunnable implements Runnable{
 
-	private RequestAttributes requestAttributes;
-	private Thread thread;
-
+	private Authentication authentication;
+	protected UsersLogged usersLogged;
 
 	public SpringRunnable(){
-		this.requestAttributes = RequestContextHolder.getRequestAttributes();
-		this.thread = Thread.currentThread();
+		this.authentication = SecurityContextHolder.getContext().getAuthentication();
 	}
-
 
 	@Override
 	public void run() {
 		try {
-			RequestContextHolder.setRequestAttributes(requestAttributes);
 			onRun();
 		} finally {
-			if (Thread.currentThread() != thread) {
-				RequestContextHolder.resetRequestAttributes();
-			}
-			thread = null;
+			cleanAuthenticationAndUser();
 		}
 	}
 
 
+	private void cleanAuthenticationAndUser() {
+		authentication = null;
+		usersLogged = null;
+	}
+
 	protected abstract void onRun();
+	
+	private Authentication getAuthentication(){
+		return authentication;
+	}
+	
+	protected UsersLogged getUserLogged(){
+		if(getAuthentication() != null){
+			CustomSpringUser customUser = (CustomSpringUser) getAuthentication().getPrincipal();
+			return new UsersLoggedImpl(customUser.getRepositoryUser());
+		}
+		return new UsersLoggedImpl();
+	}
 
 }
