88e0fbbbb4317958fcbed26b50fa1cfc770eaf9c
emanuelFernandes <emanuel_queiroga1@hotmail.com>
2016-07-07 17:27:20 +0100

    [add] Cors Filter to run localhost http response

---
+++ src/main/java/com/silicolife/anote2daemon/security/CorsFilter.java
@@ -0,0 +1,32 @@
+package com.silicolife.anote2daemon.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class CorsFilter extends GenericFilterBean {
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;       
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8181"); 
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Range, Content-Disposition, Content-Type, multipart/form-data,  Authorization, X-CSRF-TOKEN");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }           
+    }
+}
