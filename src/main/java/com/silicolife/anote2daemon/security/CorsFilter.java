10b8d59945f14254b7a1b733776c975eb7a2de6d
Hugo Costa <hcosta@silicolife.com>
2016-08-03 12:25:41 +0100

    [add] Cross filter to run browser

--- src/main/java/com/silicolife/anote2daemon/security/CorsFilter.java
+++ src/main/java/com/silicolife/anote2daemon/security/CorsFilter.java
@@ -10,14 +10,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.filter.GenericFilterBean;
-import org.springframework.web.filter.OncePerRequestFilter;
 
 public class CorsFilter extends GenericFilterBean {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;       
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8181"); 
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8888"); 
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Range, Content-Disposition, Content-Type, multipart/form-data,  Authorization, X-CSRF-TOKEN");
