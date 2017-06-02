2b4d947dffa35faea7b2c1416187de31acf04746
Hugo Costa <hcosta@silicolife.com>
2017-06-02 11:33:54 +0100

    [update] Version

--- src/main/java/com/silicolife/anote2daemon/controller/ConfigurationController.java
+++ src/main/java/com/silicolife/anote2daemon/controller/ConfigurationController.java
@@ -22,7 +22,7 @@ import com.silicolife.anote2daemon.webservice.DaemonResponse;
 @Controller
 public class ConfigurationController {
 
-	private final String version = "Version: 2.4.3.3";
+	private final String version = "Version: 2.5.0.1";
 	private final String title = "@note2daemon - The best Text Mining Tool :d (SilicoLife) "+version;
 	
 	@RequestMapping(value = "/getVersion", method = RequestMethod.GET)
