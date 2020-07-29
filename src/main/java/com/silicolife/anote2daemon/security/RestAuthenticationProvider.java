6bb2652b6f121b897427c0a6ac99c4d921b0523d
Ruben Rodrigues <ru13en_12@hotmail.com>
2020-07-29 12:28:06 +0100

    [Update] MD5 and SHA256 are now deprecated. Passwords now uses BCrypt encoder. MYSQL DB passwords hash must be upgraded to use this version!

--- src/main/java/com/silicolife/anote2daemon/security/RestAuthenticationProvider.java
+++ src/main/java/com/silicolife/anote2daemon/security/RestAuthenticationProvider.java
@@ -6,14 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
-import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import com.silicolife.anote2daemon.model.core.entities.CustomSpringUser;
 import com.silicolife.anote2daemon.service.users.UsersService;
 import com.silicolife.textmining.core.datastructures.dataaccess.database.dataaccess.implementation.exceptions.general.ExceptionsCodes;
 
@@ -26,37 +24,37 @@ import com.silicolife.textmining.core.datastructures.dataaccess.database.dataacc
 public class RestAuthenticationProvider implements AuthenticationProvider {
 
 	private UsersService userService;
-	private ShaPasswordEncoder passwordEncoder;
+	private BCryptPasswordEncoder passwordEncoder;
 
 	@Autowired
-	public RestAuthenticationProvider(UsersService userService, ShaPasswordEncoder passwordEncoder) {
+	public RestAuthenticationProvider(UsersService userService, BCryptPasswordEncoder passwordEncoder) {
 		this.userService = userService;
 		this.passwordEncoder = passwordEncoder;
 	}
 
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-		String username = authentication.getName();
-		String password = (String) authentication.getCredentials();
-		UserDetails user = userService.loadUserByUsername(username);
+		String inputUsername = authentication.getName();
+		String inputPassword = (String) authentication.getCredentials();
+		UserDetails user = userService.loadUserByUsername(inputUsername);
 		if (user == null) {
 			throw new BadCredentialsException(ExceptionsCodes.msgWrongCredentials);
 		}
-		/**
-		 * Create user salt to add security password
-		 */
-		Long userId = ((CustomSpringUser) user).getRepositoryUser().getAuId();
-		String strUserId = String.valueOf(userId);
-		Md5PasswordEncoder encoder = new Md5PasswordEncoder();
-		encoder.setIterations(13);
-		String salt = encoder.encodePassword(strUserId, null);
+//		/**
+//		 * Create user salt to add security password
+//		 */
+//		Long userId = ((CustomSpringUser) user).getRepositoryUser().getAuId();
+//		String strUserId = String.valueOf(userId);
+//		Md5PasswordEncoder encoder = new Md5PasswordEncoder();
+//		encoder.setIterations(13);
+//		String salt = encoder.encodePassword(strUserId, null);
 
-		if (!passwordEncoder.isPasswordValid(user.getPassword(), password, salt)) {
+		if (!passwordEncoder.matches(inputPassword, user.getPassword())) {
 			throw new BadCredentialsException(ExceptionsCodes.msgWrongCredentials);
 		}
 
 		Collection<? extends GrantedAuthority> authorities = user.getAuthorities();
-		return new UsernamePasswordAuthenticationToken(user, password, authorities);
+		return new UsernamePasswordAuthenticationToken(user, inputPassword, authorities);
 	}
 
 	@Override
